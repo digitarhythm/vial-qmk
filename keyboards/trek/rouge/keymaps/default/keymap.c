6b319d8e2986d610816507abe0799805512a50c1
Hajime Oh-yake <trek.kbd@gmail.com>
2026-06-24 23:08:18 +0900

    update default keymap for trek/rouge

--- keyboards/trek/rouge/keymaps/default/keymap.c
+++ keyboards/trek/rouge/keymaps/default/keymap.c
@@ -26,77 +26,18 @@ enum custom_keycodes {
     JS_CALIB_RESET                // EEPROM 消去・デフォルト値に戻す
 };
 
-/*
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, KC_BTN1, _______, _______
-    ),
-    [1] = LAYOUT(
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______
-    ),
-    [2] = LAYOUT(
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______
-    ),
-    [3] = LAYOUT(
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______
-    ),
-    [4] = LAYOUT(
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______
-    ),
-    [5] = LAYOUT(
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______
-    ),
-};
-
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-  [_BASE]    = { ENCODER_CCW_CW(KC_WH_D, KC_WH_U), ENCODER_CCW_CW(LGUI(KC_EQL), LGUI(KC_MINS)), ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
-  [_BASE2]   = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(LCTL(KC_EQL), LCTL(KC_MINS)), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-  [_LOWER]   = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-  [_RAISE]   = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-  [_ADJUST]  = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
-  [_ADJUST2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
-};
-
-#endif
-
-enum custom_keycodes {
-    Calib Start = QK_KB_0,
-    Calib End,
-    Calib Reset,
-};
-*/
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,  KC_BTN1,        LGUI(KC_0),     KC_MUTE,       LGUI(KC_C),
         MO(2),   KC_BTN1,        KC_BTN2,        KC_WH_U,       LGUI(KC_V),
         MO(5),   LGUI(KC_LBRC),  LGUI(KC_RBRC),  KC_WH_D,       KC_F3,
-        KC_SPC,  SGUI(KC_LBRC),  TG(1),          SGUI(KC_RBRC), KC_ENT
+        KC_ENT,  SGUI(KC_LBRC),  TG(1),          SGUI(KC_RBRC), LGUI(KC_TAB)
     ),
     [1] = LAYOUT(
         KC_TRNS, KC_TRNS,        LCTL(KC_0),     KC_TRNS,       LCTL(KC_C),
         MO(3),   KC_TRNS,        KC_TRNS,        KC_TRNS,       LCTL(KC_V),
-        MO(5),   LALT(KC_LEFT),  LALT(KC_RGHT),  KC_TRNS,       KC_TRNS,
-        KC_TRNS, LCTL(KC_PGUP),  KC_TRNS,        RCTL(KC_PGDN), KC_TRNS
+        MO(5),   LALT(KC_LEFT),  LALT(KC_RGHT),  KC_TRNS,       LGUI(KC_TAB),
+        KC_TRNS, LCTL(KC_PGUP),  KC_TRNS,        RCTL(KC_PGDN), LGUI(KC_TAB)
     ),
     [2] = LAYOUT(
         KC_TRNS, KC_TRNS,        KC_TRNS,        KC_TRNS,       KC_TRNS,
